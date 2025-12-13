2c7925a19bd72eea3b527813861ae8028daf2818
Venturim1 <bruno.venturim1@proton.me>
2025-12-13 18:43:37 -0300

    Criando endpoint para recebimento dos produtos

---
+++ src/main/java/com/prudoto/produtosapi/controller/ProdutoController.java
@@ -0,0 +1,19 @@
+package com.prudoto.produtosapi.controller;
+
+import com.prudoto.produtosapi.model.Produto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("produtos")
+public class ProdutoController {
+
+    @PostMapping
+    public Produto save(@RequestBody Produto produto) {
+        System.out.println("Produto salvo: " + produto);
+
+        return produto;
+    }
+}
---
+++ src/main/java/com/prudoto/produtosapi/model/Produto.java
@@ -0,0 +1,51 @@
+package com.prudoto.produtosapi.model;
+
+public class Produto {
+
+    private Long id;
+    private String nome;
+    private String Descricao;
+    private Double Preco;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return Descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        Descricao = descricao;
+    }
+
+    public Double getPreco() {
+        return Preco;
+    }
+
+    public void setPreco(Double preco) {
+        Preco = preco;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", Descricao='" + Descricao + '\'' +
+                ", Preco=" + Preco +
+                '}';
+    }
+}
