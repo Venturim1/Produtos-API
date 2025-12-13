4579f18882756886a0ab5462e0b6eebad3de7aca
Venturim1 <bruno.venturim1@proton.me>
2025-12-13 18:24:01 -0300

    Primeira rota hello-world

--- src/main/java/com/prudoto/produtosapi/ProdutoApiApplication.java
+++ src/main/java/com/prudoto/produtosapi/ProdutoApiApplication.java
@@ -2,9 +2,17 @@ package com.prudoto.produtosapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ProdutoApiApplication {
+
+	@GetMapping("/hello-world")
+	public String helloWorld() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProdutoApiApplication.class, args);
