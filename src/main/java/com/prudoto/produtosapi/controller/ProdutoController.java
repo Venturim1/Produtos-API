7b27243fd3a17dbf8db045bc19f9419183f9ad1b
Venturim1 <bruno.venturim1@proton.me>
2025-12-13 21:35:38 -0300

    Adicionando de métodos: Find, FindById e Save

--- src/main/java/com/prudoto/produtosapi/controller/ProdutoController.java
+++ src/main/java/com/prudoto/produtosapi/controller/ProdutoController.java
@@ -1,19 +1,37 @@
 package com.prudoto.produtosapi.controller;
 
 import com.prudoto.produtosapi.model.Produto;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.prudoto.produtosapi.repository.ProdutoRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("produtos")
 public class ProdutoController {
 
+    private ProdutoRepository produtoRepository;
+
+    public ProdutoController(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
     @PostMapping
     public Produto save(@RequestBody Produto produto) {
         System.out.println("Produto salvo: " + produto);
+        produtoRepository.save(produto);
 
         return produto;
+    }
+
+    @GetMapping
+    public List<Produto> findAll() {
+        return produtoRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Produto findById(@PathVariable("id") Long id) {
+        return produtoRepository.findById(id).orElse(null);
     }
 }
